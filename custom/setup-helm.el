dcf00d8f8f4dd3f40d0f3901fee79d8d0382a1f9
Russell Simpkins <russellsimpkins@gmail.com>
2022-03-21 08:16:03 -0400

    was causing problems

--- custom/setup-helm.el
+++ custom/setup-helm.el
@@ -6,7 +6,6 @@
 ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
-
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
@@ -15,8 +14,8 @@
 (define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
 (define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
 
-(when (executable-find "curl")
-  (setq helm-google-suggest-use-curl-p t))
+;; RSS(when (executable-find "curl")
+;; RSS  (setq helm-google-suggest-use-curl-p t))
 
 (setq
  helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
